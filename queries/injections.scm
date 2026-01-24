8ae749f67ea0e7c019c60156017079f4af782f09
Bob Rockefeller <bob@bobrockefeller.com>
2026-01-24 11:13:52 -0500

    Injecting YAML front matter

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,9 +1,9 @@
+((front_matter) @injection.content
+  (#set! injection.language "yaml"))
+
 ((content) @injection.content
   (#set! injection.language "html")
   (#set! injection.combined))
-
-((front_matter) @injection.content
-  (#set! injection.language "yaml"))
 
 ((code) @injection.content
   (#set! injection.language "javascript"))
